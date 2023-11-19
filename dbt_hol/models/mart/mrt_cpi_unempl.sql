210a110985fdeda694f01f81dbb24d67178b59bf
Nikita Volynets <nikita.volynets@gmail.com>
2023-11-18 16:40:02 -0800

    Initial commit

---
+++ dbt_hol/models/mart/mrt_cpi_unempl.sql
@@ -0,0 +1,9 @@
+select
+    cpi."Date"
+    , cpi.geo_id
+    , cpi.category as "Category"
+    , cpi."Value" as "CPI Index"
+from {{ref('tfm_cpi_index')}} cpi
+join {{ref('stg_geo_index')}} geo
+on cpi.geo_id = geo.geo_id
+where cpi.seasonal_adjustment = 'Y'
